085cfb2b5a3bb4722af25c85beb36ff61176b5a9
Sylvie Cattoire <cattoiresylvie.pro@gmail.com>
2022-07-29 10:23:43 +0200

    init

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+// 1 - PSEUDO CODE --------------------
+
+// Objectif ---
+
+// Solution --- 
+
+
+
+// 2 - CODE --------------------
+
+// Etape 1 ---
+
+// Etape 2 ---
+
+// Etape 3 ---
