13a5a822fc03cc398691ba4787ea7588540b18f4
Sylvie Cattoire <cattoiresylvie.pro@gmail.com>
2022-07-29 21:59:47 +0200

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,21 @@ let image = document.querySelector('.background-head');
 
 // Créer un tableau d'url d'images
 let imagesArray = ['background-head-1', 'background-head-2', 'background-head-3'];
+console.log(imagesArray[1])
 
 // Init un index
 let i = 1;
 
 // Déclenche une fonction toutes les 15 secondes qui changera l'image
-setInterval(changeImage, 600);
+setInterval(changeImage, 15000);
 
 function changeImage() {
 
-    image.style.backgroundImage = `url(../images/${imagesArray[i]}.jpg`;
+    image.style.backgroundImage = `url(./assets/images/${imagesArray[i]}.jpg)`;
     i++;
 
     if (i == imagesArray.length) {
-        i = 1;
+        i = 0;
     }
 
 }
