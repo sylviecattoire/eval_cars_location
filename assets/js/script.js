f7d30ff91564a2e84323f31df6e7d4cb6ea6173a
Sylvie Cattoire <cattoiresylvie.pro@gmail.com>
2022-07-29 19:51:33 +0200

    ouais j'ai oublié de commit ouais

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,34 @@
 // 1 - PSEUDO CODE --------------------
 
 // Objectif ---
+// L’image en haut de page doit changer toutes les 15 secondes
 
 // Solution --- 
-
+// Changer l'url du background-image via une fonction
+// Trouver le moyen de faire déclencher une fonction toutes les 15 secondes
 
 
 // 2 - CODE --------------------
 
-// Etape 1 ---
+// Cibler la div pour changer le background image
+let image = document.querySelector('.background-head');
 
-// Etape 2 ---
+// Créer un tableau d'url d'images
+let imagesArray = ['background-head-1', 'background-head-2', 'background-head-3'];
 
-// Etape 3 ---
+// Init un index
+let index = 1;
+
+// Déclenche une fonction toutes les 15 secondes qui changera l'image
+setInterval(changeImage, 600);
+
+function changeImage() {
+
+    image.style.backgroundImage = `url(/assets/images/${imagesArray[index]}.jpg`;
+    index++;
+
+    if (index == imagesArray.length) {
+        index = 1;
+    }
+
+}
